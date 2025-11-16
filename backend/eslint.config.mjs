b5b6563fbe304f8dba376d08b5befc1160b54abf
ihebmeftah <ihebmeftah666@gmail.com>
2025-11-16 12:06:55 +0100

    feat: initialize frontend with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies for React, Next.js, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and logos in the public directory.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      // relax strict TypeScript rules to reduce editor "red" errors
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/require-await': 'off'
+    },
+  },
+);
